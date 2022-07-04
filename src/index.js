2d97115bac5345492ad13eeda2823e6f5a1dcef0
Raya Shcherban <90385588+Raisiya@users.noreply.github.com>
2022-07-04 19:47:57 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,48 +1,103 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import './css/styles.css';
+import Notiflix, { Notify } from 'notiflix';
 import axios from 'axios';
-import { GetPixabayApi } from './apikey';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { GetPixabayApi } from './fetchAPI';
 
-const gallery = document.querySelector('.gallery');
-const form = document.querySelector('.search-form');
+Notify.init({
+    position:'center-top',
+    timeout: 2000,
+    cssAnimationStyle: 'from-top',
+    showOnlyTheLastOne: true,
+})
+
+const galleryRef = document.querySelector('.gallery');
+const formRef = document.querySelector('.search-form');
+const buttonRef = document.querySelector('.load-more');
+
+formRef.addEventListener('submit', onFormSubmit);
+buttonRef.addEventListener('click', onLoadMore);
+
 const getPixabayApi = new GetPixabayApi();
 
-form.addEventListener('submit', onFormSubmit);
 
-function createGallaryMarkup(images) {
-    return images.map(({ webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-    }) => `<div class="photo-card"><a href = "${largeImageURL}">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Like: ${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views: ${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments: ${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads: ${downloads}</b>
-          </p></a>
-        </div>
-      </div>`).join('');
-};
-
-function renderGallery(images) {
-  gallery.insertAdjacentHTML('beforeend', createGallaryMarkup(images));
+function makeGalleryMarkup(searchedImages) {
+  return searchedImages
+  .map(
+    ({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    vievs,
+    comments,
+    downloads,
+}) => `<div class="photo-card">
+<a href="${largeImageURL}">
+<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+<div class="info">
+  <p class="info-item">
+    <b>Likes: ${likes}</b>
+  </p>
+  <p class="info-item">
+    <b>Views: ${vievs}</b>
+  </p>
+  <p class="info-item">
+    <b>Comments: ${comments}</b>
+  </p>
+  <p class="info-item">
+    <b>Downloads: ${downloads}</b>
+  </p>
+</div>
+</a>
+</div>`
+)
+.join('');
 }
 
-function onFormSubmit (evt) {
-  evt.preventDefault();
-  getPixabayApi.fetchImages();
-  // renderGallery();
+function renderGallery(searchedImages) {
+ galleryRef.insertAdjacentHTML('beforeend', makeGalleryMarkup(searchedImages));
 }
+
+async function onFormSubmit(event) {
+    event.preventDefault();
+    clearGallery();
+    getPixabayApi.resetPage();
+    const request = event.target.elements.searchQuery.value.trim();
+    if(!request) {return Notify.info("Input some to search")}
+   
+    
+   getPixabayApi.searchQueryRequest = request;
+   try {
+    const {hits,totalHits} = await getPixabayApi.fetchImg();
+    if(!totalHits) {
+        return Notify.warning("Sorry, there are no images matching your search query. Please try again.")}
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+    renderGallery(hits);
+    // lightbox.refresh();
+   } catch(error) {console.log(error.message)}
+    
+    
+    event.target.reset();
+
+
+}
+
+async function onLoadMore() {
+
+ try{ 
+const {hits,totalHits} = await getPixabayApi.fetchImg();
+renderGallery(hits);
+// lightbox.refresh();
+} catch(error) {console.log(error.message)} 
+}
+
+function clearGallery() {
+galleryRef.innerHTML = "";
+
+}
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//    captionsData:'alt',
+//    captionDelay: 300,  
+//      });
